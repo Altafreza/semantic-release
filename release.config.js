d619d0fb727c875a2626931319c2ed3d013f37f6
Md Altaf Raja <altafbgs55@gmail.com>
2025-02-02 01:08:04 +0530

    chore: standardize semantic-release configuration

--- release.config.js
+++ release.config.js
@@ -1,19 +1,19 @@
 export default {
   branches: [
     'main',  // Production Stable Release
-    { 
-      name: 'develop', 
-      channel: 'beta', 
+    {
+      name: 'develop',
+      channel: 'beta',
       prerelease: 'beta'  // Will create versions like 1.0.0-beta.1
     },
-    { 
-      name: 'qa', 
-      channel: 'qa', 
+    {
+      name: 'qa',
+      channel: 'qa',
       prerelease: 'qa'  // Will create versions like 1.0.0-qa.1
     },
-    { 
-      name: 'uat', 
-      channel: 'uat', 
+    {
+      name: 'uat',
+      channel: 'uat',
       prerelease: 'uat'  // Will create versions like 1.0.0-uat.1
     },
     {
@@ -22,12 +22,36 @@ export default {
       prerelease: 'hotfix' // Will create versions like 1.0.1-hotfix.1
     }
   ],
-  tagFormat: '${version}',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: [
+        { type: 'feat', release: 'minor' },
+        { type: 'fix', release: 'patch' },
+        { type: 'perf', release: 'patch' },
+        { type: 'docs', release: 'patch' },
+        { type: 'style', release: 'patch' },
+        { type: 'refactor', release: 'patch' },
+        { type: 'test', release: 'patch' },
+        { type: 'build', release: 'patch' },
+        { type: 'ci', release: 'patch' },
+        { breaking: true, release: 'major' },
+        { type: 'revert', release: 'patch' }
+      ]
+    }],
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git'
-  ]
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+      changelogTitle: '# Semantic Release Changelog'
+    }],
+    ['@semantic-release/npm', {
+      npmPublish: false,
+      pkgRoot: '.'
+    }],
+    ['@semantic-release/git', {
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }]
+  ],
+  tagFormat: '${version}'
 }
