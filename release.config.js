ef686c932f635afc25b6f745b19817c04d816385
Md Altaf Raja <altafbgs55@gmail.com>
2025-02-02 17:25:40 +0530

    fix: simplify tag format

--- release.config.js
+++ release.config.js
@@ -60,6 +60,6 @@ export default {
       failComment: "The automated release failed. Please check the logs for more details."
     }]
   ],
-  tagFormat: '${version}-${prerelease.name}.${prerelease.number}',
+  tagFormat: '${version}-${branch}',
   preset: 'angular'
 }
